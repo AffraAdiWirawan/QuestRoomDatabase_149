e004fb5e7bd58f226529affec7cb9cb0454cf227
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-17 02:36:53 +0700

    Membuat MahasiswaDao untuk insert update dan delete

--- app/src/main/java/com/example/activity11/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/activity11/data/dao/MahasiswaDao.kt
@@ -6,6 +6,19 @@ import com.example.activity11.data.entity.Mahasiswa
 
 @Dao
 interface MahasiswaDao {
+
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun  getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
