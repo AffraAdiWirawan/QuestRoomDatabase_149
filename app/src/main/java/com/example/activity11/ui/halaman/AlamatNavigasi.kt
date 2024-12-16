72315ea9807b94583ce0315cf72eeefb0fede5f3
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-17 03:05:59 +0700

    Mendefinisikan objek singleton DestinasiHome yang mewakili rute navigasi untuk halaman "Home".

--- app/src/main/java/com/example/activity11/ui/halaman/AlamatNavigasi.kt
+++ app/src/main/java/com/example/activity11/ui/halaman/AlamatNavigasi.kt
@@ -3,6 +3,9 @@ package com.example.activity11.ui.halaman
 interface AlamatNavigasi {
     val route: String
 
-    
+    object DestinasiHome : AlamatNavigasi {
+        override val route = "home"
+    }
+
 
 }
